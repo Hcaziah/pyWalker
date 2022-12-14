271090f99e0dc137b344676865d65f10e6a62922
Hcaziah <esteppzachary@live.com>
2022-12-13 22:14:53 -0500

    init push

---
+++ char.py
@@ -0,0 +1,9 @@
+import pygame as pyg
+
+class Char:
+    def __init__(self) -> None:
+        self.shape_list = []
+        
+
+    def get_shape(self):
+        return self.shape_list
---
+++ main.py
@@ -0,0 +1,16 @@
+import pygame as pyg
+
+
+def main():
+    pyg.init()
+    window = pyg.display.set_mode((800, 600))
+
+    pyg.display.pygame.display.set_caption("pyWalker", icontitle=None)
+
+    screen = pyg.display.get_surface()
+    
+
+
+
+if __name__ == "__main__":
+    main()
